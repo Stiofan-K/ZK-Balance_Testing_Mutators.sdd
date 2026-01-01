3478725062f3181b6cff12b4037af15fc18b05ff
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-01-01 20:46:38 +0100

    Slow start

---
+++ units/amphtankcon.lua
@@ -0,0 +1,149 @@
+return { amphtankcon = {
+  name                   = [[Welder]],
+  description            = [[Armed Construction Tank]],
+  acceleration           = 0.4,
+  brakeRate              = 18.0,
+  buildDistance          = 180,
+  builder                = true,
+
+  buildoptions           = {
+  },
+
+  buildPic               = [[tankcon.png]],
+  canGuard               = true,
+  canMove                = true,
+  canPatrol              = true,
+  category               = [[LAND SHIP]],
+  collisionVolumeOffsets = [[0 6 0]],
+  collisionVolumeScales  = [[34 18 46]],
+  collisionVolumeType    = [[box]],
+  corpse                 = [[DEAD]],
+
+  customParams           = {
+    aimposoffset   = [[0 0 0]],
+    midposoffset   = [[0 -4 0]],
+    modelradius    = [[30]],
+    selection_scale = 1.5,
+    normaltex = [[unittextures/welder_normals.dds]],
+
+    outline_x = 80,
+    outline_y = 80,
+    outline_yoff = 12.5,
+  },
+
+  explodeAs              = [[BIG_UNITEX]],
+  footprintX             = 2,
+  footprintZ             = 2,
+  health                 = 1700,
+  iconType               = [[builder]],
+  leaveTracks            = true,
+  maxSlope               = 18,
+  maxWaterDepth          = 22,
+  metalCost              = 185,
+  movementClass          = [[TANK3]],
+  moveState              = 0,
+  noAutoFire             = false,
+  noChaseCategory        = [[TERRAFORM FIXEDWING SATELLITE SUB]],
+  objectName             = [[welder.s3o]],
+  script                 = [[amphtankcon.lua]],
+  selfDestructAs         = [[BIG_UNITEX]],
+
+  sfxtypes               = {
+
+    explosiongenerators = {
+      [[custom:BEAMWEAPON_MUZZLE_RED]],
+    },
+
+  },
+
+  showNanoSpray          = false,
+  sightDistance          = 300,
+  speed                  = 63,
+  trackOffset            = 3,
+  trackStrength          = 6,
+  trackStretch           = 1,
+  trackType              = [[StdTank]],
+  trackWidth             = 38,
+  turninplace            = 0,
+  turnRate               = 1000,
+  workerTime             = 7.5,
+
+  weapons                = {
+
+    {
+      def                = [[LASER]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+  },
+
+
+  weaponDefs             = {
+
+    LASER = {
+      name                    = [[Mini Laser]],
+      areaOfEffect            = 8,
+      cegTag                  = [[laser_cannon_trail_thin]],
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customParams        = {
+        light_camera_height = 1200,
+        light_radius = 120,
+      },
+
+      damage                  = {
+        default = 8.64,
+        planes  = 8.64,
+      },
+
+      duration                = 0.02,
+      explosionGenerator      = [[custom:BEAMWEAPON_HIT_RED]],
+      fireStarter             = 50,
+      heightMod               = 1,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      noSelfDamage            = true,
+      range                   = 235,
+      reloadtime              = 0.2,
+      rgbColor                = [[1 0 0]],
+      soundHit                = [[weapon/laser/lasercannon_hit]],
+      soundStart              = [[weapon/laser/lasercannon_fire]],
+      soundTrigger            = true,
+      thickness               = 2.5,
+      tolerance               = 10000,
+      turret                  = true,
+      weaponType              = [[LaserCannon]],
+      weaponVelocity          = 880,
+    },
+
+  },
+
+
+  featureDefs            = {
+
+    DEAD  = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 3,
+      footprintZ       = 3,
+      object           = [[welder_dead.s3o]],
+      collisionVolumeOffsets        = [[0 -2 0]],
+      collisionVolumeScales         = [[34 18 46]],
+      collisionVolumeType           = [[box]],
+    },
+
+    HEAP  = {
+      blocking         = false,
+      footprintX       = 3,
+      footprintZ       = 3,
+      object           = [[debris3x3b.s3o]],
+    },
+
+  },
+
+} }
