bbb36bc802ee25b0cf6cf465afe18ffaf7a871b7
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-04-06 16:28:33 +0200

    v 1.3.18
    just herc drop not doing damage

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Stiofan Balance Refumble',
 	description = 'Playing around with balance to feel things out!',
 	shortname = 'zkstiofanbalancerefumble',
-	version = 'v1.3.17 dev',
+	version = 'v1.3.18 dev',
 	mutator = '1',
 	game = '',
 	shortGame = '',
