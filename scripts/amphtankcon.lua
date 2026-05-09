8ac4add76ff91668f4fd91c3ee68e1fae07391ae
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-05-09 23:31:47 +0200

    Murderhole model!
    Murderhole model, animations and wreck
    
    Luckily can borrow just a little from welders texturing somehow :O
    
    still needs texturing done, as I don't quite get how it needs to be done yet

--- scripts/amphtankcon.lua
+++ scripts/amphtankcon.lua
@@ -1,16 +1,26 @@
 include "constants.lua"
 
-local base, nano, guns, doors, turret, shovel = piece ('base', 'nano', 'guns', 'doors', 'turret', 'shovel')
+local base, body = piece ('base','body')
+
+-- Doors and Propeller
+local  ldoor, rdoor, propeller = piece('ldoor','rdoor','propeller')
+
+-- Wheels
+local wheelbase, lwheelchassis, rwheelchassis = piece ('wheelbase', 'lwheelchassis', 'rwheelchassis')
+local lwheels = { piece 'lwheel1', piece 'lwheel2', piece 'lwheel3', piece 'lwheel4'}
+local rwheels = { piece 'rwheel1', piece 'rwheel2', piece 'rwheel3', piece 'rwheel4'}
+
+-- Turret
+local turretweapon,turretbarrel, firepoint = piece ('turretgun','turretbarrel','firepoint')
 
 -- Construction
-
-local nanos = { piece 'nano1', piece 'nano2' }
+local turretnano = piece('turretnano')
+local nanopoints = { piece 'lnanopoint', piece 'rnanopoint' }
 local SIG_BUILD = 1
 
-function script.StartBuilding(heading)
+function script.StartBuilding(heading, pitch)
 	Signal (SIG_BUILD)
-	Turn (doors, x_axis, math.rad(-100), math.rad(200))
-	Move (nano, z_axis, 3, 12)
+	Turn (turretnano, z_axis, heading, math.rad(150))
 	Spring.SetUnitCOBValue(unitID, COB.INBUILDSTANCE, 1)
 end
 
@@ -18,15 +28,16 @@ function script.StopBuilding()
 	Spring.SetUnitCOBValue(unitID, COB.INBUILDSTANCE, 0)
 	SetSignalMask (SIG_BUILD)
 	Sleep (5000)
-	Turn (doors, x_axis, 0, math.rad(200))
-	Move (nano, z_axis, 0, 12)
 end
 
 -- Weaponry
 
-local flares = { piece 'flare1', piece 'flare2' }
-local current_flare = 1
+local turretweapon, turretbarrel, firepoint = piece ('turretgun','turretbarrel','firepoint')
 local SIG_AIM = 2
+
+local TURRET_TURN_SPEED = math.rad(270)
+local SLEEVE_TURN_SPEED = math.rad(90)
+
 
 local function PrioritiseTorpGun()
 	torpAim = true
@@ -35,21 +46,35 @@ local function PrioritiseTorpGun()
 end
 
 function script.QueryWeapon(num)
-	return turret
+	return firepoint
 end
 
 function script.AimFromWeapon(num)
-	return turret
+	return firepoint
 end
 
 function script.AimWeapon(num, heading, pitch)
-	if num == 1 then
-		StartThread(PrioritiseTorpGun)
-	elseif num == 2 and torpAim then
-		return false
-	end
 	Signal(SIG_AIM)
 	SetSignalMask(SIG_AIM)
+	
+	Turn(turretweapon, z_axis, heading, TURRET_TURN_SPEED)
+	Turn(turretbarrel, x_axis, -pitch, SLEEVE_TURN_SPEED)
+	WaitForTurn(turretweapon, z_axis)
+	
+	-- TODO Restore default would be good
+	--StartThread(RestoreAfterDelay)
+	
+	if num == 1 then
+		StartThread(PrioritiseTorpGun)
+
+		
+	elseif num == 2 and not torpAim then
+		Turn(turretbarrel, x_axis, -pitch, SLEEVE_TURN_SPEED)
+		WaitForTurn(turretbarrel, x_axis)
+		return true
+	else 
+		return false
+	end
 	return true
 end
 
@@ -59,14 +84,10 @@ function script.FireWeapon(num)
 		GG.UpdateUnitAttributes(unitID)
 	end
 end
+
 function script.BlockShot(num, targetID)
-	-- Blocking torplauncher shots that are unlikely to get into water
-	-- I just like the astethics, even if firing like the cannon would be more effective
-	local x,y,z = Spring.GetUnitPosition(unitID)
-	if num == 1 and y > -10 then
-		torpAim = false
-		return true
-	end
+	-- I just like the astethics of tossing a charge out of the back, even if firing like the cannon would be more effective
+	--> Still think the above to be true, but since you now have a turret you should use it
 	local reloadState = Spring.GetUnitWeaponState(unitID, 1, 'reloadState')
 	return not (reloadState and (reloadState < 0 or reloadState < Spring.GetGameFrame()))
 end
@@ -75,51 +96,99 @@ end
 
 -- Misc
 
+local wheelTurnSpeed = 1
+
+function script.StartMoving()
+	moving = true
+	local x,y,z = Spring.GetUnitVelocity(unitID)
+	wheelTurnSpeed = math.sqrt(x*x+y*y+z*z)*10
+	
+	for i,wheel in ipairs(lwheels) do
+		Spin(wheel, x_axis, wheelTurnSpeed)
+	end
+	for i,wheel in ipairs(rwheels) do
+		Spin(wheel, x_axis, wheelTurnSpeed)
+	end
+	Spin(propeller, y_axis, wheelTurnSpeed*3)
+end
+
+function script.StopMoving()
+	moving = false
+	for i,wheel in ipairs(lwheels) do
+		StopSpin(wheel,x_axis)
+	end
+	for i,wheel in ipairs(rwheels) do
+		StopSpin(wheel,x_axis)
+	end
+	StopSpin(propeller, y_axis)
+end
+
+local function AmphibControl(wantAmbhib)
+	if wantAmbhib then
+		Turn(lwheelchassis, y_axis, -100, math.rad(60))
+		Turn(rwheelchassis, y_axis, 100, math.rad(60))
+		Move(wheelbase, z_axis, 2.5, 1)
+		Turn(ldoor, z_axis, 300, math.rad(60))
+		Turn(rdoor, z_axis, -300, math.rad(60))
+		WaitForTurn(ldoor, z_axis)
+		WaitForTurn(rdoor, z_axis)
+		Move(propeller, y_axis, 4, 3)
+	else
+		Turn(lwheelchassis, y_axis, 0, math.rad(60))
+		Turn(rwheelchassis, y_axis, 0, math.rad(60))
+		Move(wheelbase, z_axis, 0, 5)
+		Move(propeller, y_axis, 0, 5)
+		WaitForMove(propeller,y_axis)
+		Turn(ldoor, z_axis, 0, math.rad(60))
+		Turn(rdoor, z_axis, 0, math.rad(60))
+	end
+end
+
 local function Wake()
 	Signal(SIG_MOVE)
 	SetSignalMask(SIG_MOVE)
 	while true do
-		if not Spring.GetUnitIsCloaked(unitID) and select(2, Spring.GetUnitPosition(unitID)) <= 0 and moving then			
-			EmitSfx(base, 2)
-			EmitSfx(shovel, 2)
+		if select(2, Spring.GetUnitPosition(unitID)) <= 0 then			
+			StartThread(AmphibControl,true)
+			
+			if not Spring.GetUnitIsCloaked(unitID) and moving then	
+				EmitSfx(propeller, 2)
+				EmitSfx(lwheelchassis, 2)
+				EmitSfx(rwheelchassis, 2)
+			end
+		else
+			StartThread(AmphibControl,false)
 		end
-		Sleep(200)
+		Sleep(100)
 	end
 end
 
 function script.Create()
-	Hide(guns)
-	Hide(turret)
-	--setting up turret as firepoint
-	Turn(turret, x_axis, 90)
-	Turn(turret, y_axis, math.rad(-20))
+	StartThread(GG.Script.SmokeUnit, unitID, {body})
+	Spring.SetUnitNanoPieces(unitID, nanopoints)
 	
-	Move(turret, y_axis, 0)
-	Move(turret, z_axis, -6)
-	Move(turret, x_axis, 2)
-	
-	StartThread(GG.Script.SmokeUnit, unitID, {base})
-	Spring.SetUnitNanoPieces(unitID, nanos)
+	local x,y,z = Spring.GetUnitVelocity(unitID)
+	wheelTurnSpeed = math.sqrt(x*x+y*y+z*z)*10
 	
 	moving = false
 	StartThread(Wake)
 end
 
-local explodables = { shovel }
+
+local explodables = { turretnano, turretweapon, lwheelchassis, turretbarrel, piece('lwheel1'), ldoor, propeller }
 function script.Killed(recentDamage, maxHealth)
 	local severity = recentDamage / maxHealth
-	local brutal = (severity > 0.5)
-
-	for i = 1, #explodables do
+	local brutal = severity > 0.5
+	for i = 1, #explodables-1 do
 		if math.random() < severity then
-			Explode (explodables[i], SFX.FALL + (brutal and (SFX.SMOKE + SFX.FIRE) or 0))
+			Explode(explodables[i], SFX.FALL + (brutal and (SFX.SMOKE + SFX.FIRE) or 0))
 		end
 	end
 
 	if not brutal then
 		return 1
 	else
-		Explode (base, SFX.SHATTER)
+		Explode(body, SFX.SHATTER)
 		return 2
 	end
 end
