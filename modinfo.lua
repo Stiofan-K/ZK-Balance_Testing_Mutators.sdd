7b4b8514c3c9a465d68e51b2ae33df930b0c521c
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-05-15 21:48:06 +0200

    v1.4.2
    Fixing anura not leading at all/not retargeting it's shot

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Stiofan Balance Refumble',
 	description = 'Playing around with balance to feel things out!',
 	shortname = 'zkstiofanbalancerefumble',
-	version = 'v1.4.1',
+	version = 'v1.4.2',
 	mutator = '1',
 	game = '',
 	shortGame = '',
