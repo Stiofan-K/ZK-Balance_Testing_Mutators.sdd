b698b12279433d029a4e0ad150393a957ddf521a
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-05-15 13:11:56 +0200

    v1.4.1
    Some new interesting stuff:
    Gridding is easier to do, centralised E is stronger.
    
    Reef's missile is stronger but more expensive and slower to stock
    
    Anura is a bit weaker

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Stiofan Balance Refumble',
 	description = 'Playing around with balance to feel things out!',
 	shortname = 'zkstiofanbalancerefumble',
-	version = 'v1.3.22 dev',
+	version = 'v1.4.1',
 	mutator = '1',
 	game = '',
 	shortGame = '',
