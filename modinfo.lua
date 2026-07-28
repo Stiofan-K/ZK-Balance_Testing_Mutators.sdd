2b971fc6625ab78da3c75b9d1ac03d52c5f7c8e2
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-07-28 23:41:26 +0200

    V 1.5.5 Keeping up with birdsnest
    Heron and Falcon want to aproach more to keep their attackrun going

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Stiofan Balance Refumble',
 	description = 'Playing around with balance to feel things out!',
 	shortname = 'zkstiofanbalancerefumble',
-	version = 'v1.5.4',
+	version = 'v1.5.5',
 	mutator = '1',
 	game = '',
 	shortGame = '',
