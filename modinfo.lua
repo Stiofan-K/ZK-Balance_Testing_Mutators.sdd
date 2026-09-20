d3819a4cfbc98648883ffd54aa7f3e14285eef18
Stiofan-K <108685389+Stiofan-K@users.noreply.github.com>
2026-09-20 11:22:54 +0200

    v 1.6.0
    Hive Torpedoes
    Croc VLS instead of torp
    Nebula nerf/adjustment
    
    Airplane nest discards some options and takes on some base plane fac planes to round out capability
    
    Shield changes dropped to let base do its thing.
    phoenix things also shelved

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Stiofan Balance Refumble',
 	description = 'Playing around with balance to feel things out!',
 	shortname = 'zkstiofanbalancerefumble',
-	version = 'v1.5.5',
+	version = 'v1.6.0',
 	mutator = '1',
 	game = '',
 	shortGame = '',
